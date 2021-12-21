abbfff1db78a637c964fb7b4d6bab71a992ae730
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-21 14:00:59 -0500

    added POST route to write data from body to feedback.json file

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -4,8 +4,35 @@
 // just as the code executed in getStaticProps and getServerSideProps was
 // not exposed to the client
 
+import fs from 'fs'
+import path from 'path'
+
 function handler(req, res) {
-    res.status(200).json({message: 'This works!'})
+    if (req.method === 'POST') {
+        // Next automatically parses body data
+        const email = req.body.email
+        const feedbackText = req.body.text
+        // console.log('request email:', email, 'request feedback:', feedbackText);
+        // console.log(req.method === 'POST');
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        }
+        // we will store data in a file but could store in a database
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+        // this reads and gives us the current data in the file
+        const fileData = fs.readFileSync(filePath)
+        const data = JSON.parse(fileData)
+        data.push(newFeedback)
+        fs.writeFileSync(filePath, JSON.stringify(data))
+
+        res.status(201).json({message: 'Successfully sent feedback!', feedback: newFeedback})
+
+    } else {
+        res.status(200).json({message: 'This works!'})
+    }
+    
 }
 
 export default handler
