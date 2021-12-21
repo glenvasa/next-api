0157bef54fac9fe6d32e82b5759901190cab89b5
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-21 15:07:45 -0500

    refactored code and added response code for GET requests to api/feedback

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -7,30 +7,40 @@
 import fs from 'fs'
 import path from 'path'
 
+function buildFeedbackPath() {
+    return path.join(process.cwd(), 'data', 'feedback.json')
+    
+}
+
+function extractFeedback(filePath) {
+    // this reads and gives us the current data in the file
+    const fileData = fs.readFileSync(filePath)
+    const data = JSON.parse(fileData)
+    return data
+}
+
 function handler(req, res) {
     if (req.method === 'POST') {
         // Next automatically parses body data
         const email = req.body.email
         const feedbackText = req.body.text
-        // console.log('request email:', email, 'request feedback:', feedbackText);
-        // console.log(req.method === 'POST');
         const newFeedback = {
             id: new Date().toISOString(),
             email: email,
             text: feedbackText
         }
         // we will store data in a file but could store in a database
-        const filePath = path.join(process.cwd(), 'data', 'feedback.json')
-        // this reads and gives us the current data in the file
-        const fileData = fs.readFileSync(filePath)
-        const data = JSON.parse(fileData)
+        const filePath = buildFeedbackPath()
+        const data = extractFeedback(filePath)
         data.push(newFeedback)
         fs.writeFileSync(filePath, JSON.stringify(data))
 
         res.status(201).json({message: 'Successfully sent feedback!', feedback: newFeedback})
 
     } else {
-        res.status(200).json({message: 'This works!'})
+        const filePath = buildFeedbackPath()
+        const data = extractFeedback(filePath)
+        res.status(200).json({ feedback: data})
     }
     
 }
