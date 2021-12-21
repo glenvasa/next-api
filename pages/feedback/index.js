d63214ceaf11f7814c0e1d0d2a478ab8cc1c1c0d
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-21 15:56:20 -0500

    added buttton on FeedbackPage to load email address based on feedback id from dynamic [feedbackId].js api route

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,25 +1,47 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
   return (
-    <ul>
+      <Fragment>
+          {feedbackData && <p>{feedbackData.email}</p>}
+          <ul>
       {props.feedbackItems.map((item) => (
-        <li key={item.id}>{`${item.email} said: ${item.text}`}</li>
+        //   bind allows us to preconfigure a function for later use
+        // the first argument is 'this' which we don't need here; second argument is the first fn parameter to be passed
+        <li key={item.id}>
+          {item.text}{" "}
+          <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+            Show Email Address
+          </button>
+        </li>
       ))}
     </ul>
+      </Fragment>
+    
   );
 }
 
 export async function getStaticProps() {
   // you should not use fetch API inside of getStaticProps or getServerSideProps to talk to your own API
-  // just import node code and directly run it 
+  // just import node code and directly run it
   // this allows you to leverage the fact that all same project/same server
 
-  const filePath = buildFeedbackPath()
-  const data = extractFeedback(filePath)
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
 
   return {
-    props: { feedbackItems: data},
+    props: { feedbackItems: data },
   };
 }
 
